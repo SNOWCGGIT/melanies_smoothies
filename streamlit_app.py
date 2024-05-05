20392ca0970aac89a5256ffb37d66cd80394221f
SNOWCGGIT <saikumarpati123@gmail.com>
2024-05-04 22:29:26 -0400

    Update streamlit_app.py
    Made changes removed comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -34,6 +34,8 @@ if ingredients_list:
 
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen + '  '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -48,10 +50,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success("Your Smoothie is ordered  ,"+name_on_order+" !", icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
+
+
 
 
 
